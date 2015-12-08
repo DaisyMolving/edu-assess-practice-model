f61532fd650ea9dcdfc8678fbbb581acba27a2be
Daisy Molving <daisymolving@gmail.com>
2015-12-08 15:06:02 +0000

    added routes, created html erb file for teachers as index, added neat as gem

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,2 +1,59 @@
 class TeachersController < ApplicationController
+  class UsersController < ApplicationController
+
+  def index
+    @teachers = Teacher.all
+  end
+
+#   # this is a GET
+#   def show
+#     @user = current_user
+#     @projects = current_user.projects  
+#   end
+
+#   # GET
+#   def new
+#     @user = User.new
+#     @projects = Project.all
+#   end
+
+#   # POST 
+#   def create
+#     User.create(user_params)
+#     redirect_to(users_path)
+#   end
+
+#     # GET
+#   def edit
+#     @user = User.find(params[:id])
+#     @projects = Project.all
+#   end
+
+#     # PUT
+#   def update
+#     @user = User.find(params[:id])
+#     @user.update(user_params)
+#     redirect_to(user_path)
+#   end
+
+
+#    # DELETE
+#   def destroy
+#     user = User.find(params[:id])
+#     user.destroy
+#     redirect_to(users_path)
+#   end
+
+#   def delete_project
+#     user = User.find(params[:id])
+#     home.projects.delete(Projects.find(params[:id]))
+#   end
+ 
+ 
+# private
+#   def user_params
+#     params.require(:user).permit(:name, {plant_ids: []})
+#   end
+end
+
 end
